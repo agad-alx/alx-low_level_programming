8f4df19817db44035b3e0a7a5dc597309c31cc40
agad-alx <agad.code@gmail.com>
2023-08-10 16:36:42 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
-echo '#include <stdio.h>' > puts.c
-echo 'int main() { puts("Programming is like building a multilingual puzzle"); return 0; }' >> puts.c
+echo '#include <stdio.h>' > program.c
+echo 'int main() { puts("Programming is like building a multilingual puzzle"); return 0; }' >> program.c
