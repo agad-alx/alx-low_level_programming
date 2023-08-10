bbe343891b0d579911984a354a43dc508d6ae4c5
agad-alx <agad.code@gmail.com>
2023-08-10 16:59:56 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,6 @@
  */
 int main(void)
 {
-    /*
-     * Use the puts function to print the message to the console.
-     * The puts function automatically appends a newline character.
-     */
     puts("\"Programming is like building a multilingual puzzle");
     return (0); /* Return 0 to indicate successful program execution */
 }
