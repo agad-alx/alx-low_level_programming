6eab4f4a663d3918ad5a48a8577438803ea82c03
agad-alx <agad.code@gmail.com>
2023-08-10 16:51:56 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
     /*
      * Use the puts function to print the message to the console.
      * The puts function automatically appends a newline character.
      */
     puts("Programming is like building a multilingual puzzle");
 
-    return 0; /* Return 0 to indicate successful program execution */
+    return (0); /* Return 0 to indicate successful program execution */
 }
