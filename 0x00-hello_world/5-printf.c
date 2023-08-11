0646f0b4b02b474d3137a9f9976f9101000729ac
agad-alx <agad.code@gmail.com>
2023-08-10 17:21:41 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    /* Use printf to print the message with proper grammar */
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+
+    return (0); /* Return 0 to indicate successful program execution */
+}
