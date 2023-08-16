93a08ff5fec4ba48016741857a534ae304bb4b13
agad-alx <agad.code@gmail.com>
2023-08-15 18:37:13 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int fib1 = 1, fib2 = 2, next_fib;
+	int count;
+
+	printf("%d, %d", fib1, fib2);
+
+	for (count = 3; count <= 50; count++)
+	{
+		next_fib = fib1 + fib2;
+		printf(", %d", next_fib);
+
+		fib1 = fib2;
+		fib2 = next_fib;
+	}
+
+	printf("\n");
+
+	return (0);
+}
