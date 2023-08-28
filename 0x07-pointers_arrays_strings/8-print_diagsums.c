d650fa119e57b3cd8c9a66229639099e567125c6
agad-alx <agad.code@gmail.com>
2023-08-28 06:25:58 -0700

    0x07. C - Even more pointers, arrays and strings (Mandatory Tasks 0-7)

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, d;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (d = 0; d < size; d++)
+	{
+		sum1 = sum1 + a[d * size + d];
+	}
+
+	for (d = size - 1; d >= 0; d--)
+	{
+		sum2 += a[d * size + (size - d - 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
