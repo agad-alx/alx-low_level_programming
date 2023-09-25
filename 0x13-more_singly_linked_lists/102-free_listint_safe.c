98fb6d14d86e819c3db1dd4d5fe00e310d034f2c
agad-alx <agad.code@gmail.com>
2023-09-25 12:14:32 -0700

    0x13. C - More singly linked lists (Mandatory and Advanced Tasks 0-14)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,37 +1,38 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t linked list.
- * @h: Double pointer to the head of the list.
+ * free_listint_safe - Frees a listint_t list.
+ * @h: Pointer to a pointer to the head of the list.
  *
  * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current = *h;
-	listint_t *next;
-	size_t count = 0;
+	listint_t *current, *temp;
+	size_t size = 0;
 
+	/* Check if h is NULL or *h is NULL (empty list) */
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
+
+	/* Initialize a loop to go through the list */
 	while (current != NULL)
 	{
-		next = current->next;
-		printf("[%p] %d\n", (void *)current, current->n);
-		free(current);
-		count++;
+		size++;  /* Increment the size counter */
+		temp = current;  /* Store the current node */
+		current = current->next;  /* Move to the next node */
+		free(temp);  /* Free the stored node */
 
-		/* Check if the next node points back to a previous node */
-		if (next >= current)
-		{
-			printf("-> [%p] %d\n", (void *)next, next->n);
+		/* Check if we've reached the end of the original list */
+		if (current == *h)
 			break;
-		}
-
-		current = next;
 	}
 
+	/* Set the head to NULL to indicate the list has been freed */
 	*h = NULL;
 
-	return (count);
+	return (size);
 }
