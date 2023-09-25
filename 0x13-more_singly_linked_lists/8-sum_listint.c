958d9823fea659eadfd9439caf9605de295b5162
agad-alx <agad.code@gmail.com>
2023-09-25 13:36:00 -0700

    0x13. C - More singly linked lists (Mandatory and Advanced Tasks 0-14)

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,21 +1,16 @@
 #include "lists.h"
 
 /**
- * sum_listint - Returns the sum of all the data (n) of a listint_t linked list.
+ * sum_listint-Returns the sum of all the data (n) of a listint_t linked list.
  * @head: Pointer to the head of the list.
  *
- * Return: The sum of all the data, or 0 if the list is empty.
+ * Return: The sum of all the data
+ * or 0 if the list is empty.
  */
 int sum_listint(listint_t *head)
 {
-	int sum = 0;
-	listint_t *current = head;
+	if (head == NULL)
+		return (0);
 
-	while (current != NULL)
-	{
-		sum += current->n;
-		current = current->next;
-	}
-
-	return (sum);
+	return (head->n + sum_listint(head->next));
 }
