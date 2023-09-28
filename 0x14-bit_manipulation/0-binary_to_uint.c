59bcb51a99a619a8dfdd1b22ccdcbf4b80e16651
agad-alx <agad.code@gmail.com>
2023-09-28 16:38:25 -0700

    0x14. C - Bit manipulation (Mandatory to Advanced Tasks)

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,8 @@
  * binary_to_uint - Converts a binary number to an unsigned int.
  * @b: A pointer to a string of 0 and 1 chars.
  *
- * Return: The converted number, or 0 if there are invalid characters or b is NULL.
+ * Return: The converted number
+ * or 0 if there are invalid characters or b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
